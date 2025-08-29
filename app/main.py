5f3a6af22ab7f96e7ce642cd1f4022fa6fa6f84e
Vitaliy-Knysh <mr.knyshjr2@gmail.com>
2025-08-29 09:01:00 +0300

    Добавил readme, проверку ввода числа этажей, дополнил requirements.txt

--- app/main.py
+++ app/main.py
@@ -6,7 +6,16 @@ if __name__ == "__main__":
     if debug_argument():
         floor_count = 5
     else:
-        floor_count = int(input('Добро пожаловать в симулятор лифта!\nВведите количество этажей: '))
+        floor_count = 0
+        print('Добро пожаловать в симулятор лифта!\n')
+        while floor_count < 1:
+            try:
+                floor_count = int(input('Введите количество этажей: '))
+                if floor_count < 1:
+                    print('Некорректное количество этажей.')
+            except (ValueError, NameError):
+                print('Некорректное количество этажей.')
+
     elevator = Elevator(floor_count=floor_count)
     view = View(floor_count=floor_count)
     view.run(elevator=elevator)

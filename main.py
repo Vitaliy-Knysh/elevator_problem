75d96502259e9e07708e580b0cbe04a7bde9aa2b
Vitaliy <mr.knyshjr2@gmail.com>
2025-09-01 15:18:21 +0300

    исправил ошибку, когда отказывалось запускаться

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from app.model.elevator import Elevator
 from app.utils.argument_parser import debug_argument
 from app.view.view import View
 
-if __name__ == "__main__":
+
+def get_floor_count() -> int:
     if debug_argument():
         floor_count = 5
     else:
@@ -15,7 +16,11 @@ if __name__ == "__main__":
                     print('Некорректное количество этажей.')
             except (ValueError, NameError):
                 print('Некорректное количество этажей.')
+    return floor_count
 
+
+if __name__ == "__main__":
+    floor_count = get_floor_count()
     elevator = Elevator(floor_count=floor_count)
     view = View(floor_count=floor_count)
     view.run(elevator=elevator)
